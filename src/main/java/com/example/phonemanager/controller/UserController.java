054c823cd4fb4ba02c2bf579ccb0b71dbdb44b7b
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-09 20:29:55 +0200

    cerated user controller

--- src/main/java/com/example/phonemanager/controller/UserController.java
+++ src/main/java/com/example/phonemanager/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.example.phonemanager.controller;
 
-public class UserController {
+import com.example.phonemanager.entities.User;
+import com.example.phonemanager.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController extends ContactController {
+    @Autowired
+    ContactService contactService;
+
+
 }

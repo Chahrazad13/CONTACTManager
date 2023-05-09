33fad859ca5d43d050f87255376793531c905e48
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-09 16:03:38 +0200

    added CommandLineRunner method to test the project

--- src/main/java/com/example/phonemanager/PhoneManagerApplication.java
+++ src/main/java/com/example/phonemanager/PhoneManagerApplication.java
@@ -1,13 +1,33 @@
 package com.example.phonemanager;
 
+import com.example.phonemanager.entities.Contact;
+import com.example.phonemanager.repositories.ContactRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class PhoneManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhoneManagerApplication.class, args);
+
     }
+    @Bean
+    CommandLineRunner start(ContactRepository contactRepository){
+        return args -> {
+            Stream.of("Mohamed","Hassan","Najat")
+                    .forEach(name->{
+                        Contact contact=new Contact();
+                        contact.setId(contact.getId());
+                        contact.setFirstname(name);
+                        contact.setLastname("test");
+                        contactRepository.save(contact);
+                    });
+        };}
 
 }

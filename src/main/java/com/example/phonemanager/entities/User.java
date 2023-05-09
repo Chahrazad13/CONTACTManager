7dbe4c411596fab36d8d9c9d5455e39d32b013ca
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-09 16:22:08 +0200

    Created User and userRepo + set ManyToMany Relation w/ Contact

---
+++ src/main/java/com/example/phonemanager/entities/User.java
@@ -0,0 +1,23 @@
+package com.example.phonemanager.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Collection;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @Getter @Setter
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String imageURL;
+    @ManyToMany(mappedBy = "users", cascade = CascadeType.ALL)
+    private Collection<Contact> contacts;
+}

f7208c0cd03e1f9efb609a85dc4a6fb16827e734
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-09 11:20:59 +0200

    created Contact service

---
+++ src/main/java/com/example/phonemanager/service/ContactService.java
@@ -0,0 +1,32 @@
+package com.example.phonemanager.service;
+
+import com.example.phonemanager.entities.Contact;
+import com.example.phonemanager.repositories.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContactService {
+    @Autowired
+    ContactRepository contactRepository;
+
+    public Optional<Contact> findbyId(Long id){
+        return contactRepository.findById(id);
+    }
+
+    public void save(Contact contact){
+        contactRepository.save(contact);
+    }
+
+    public List<Contact> findAll(){
+        List<Contact> contacts = new ArrayList<>();
+        return contacts = contactRepository.findAll();}
+
+    public void delete(Contact contact){
+        contactRepository.delete(contact);
+    }
+}
